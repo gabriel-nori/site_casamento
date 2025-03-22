52c42bd0b98bc452d4ddda3d6e00ee0104a7a72a
gabriel.nori <gabrielnori@gabrielnori.tech>
2025-03-21 21:58:06 -0300

    Feat: create base skeleton to build

--- gabe_e_giu/src/app/app.routes.ts
+++ gabe_e_giu/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { DicasComponent } from './dicas/dicas.component';
+import { RecomendacoesComponent } from './recomendacoes/recomendacoes.component';
+import { PresentesComponent } from './presentes/presentes.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'dicas', component: DicasComponent },
+  { path: 'recomendacoes', component: RecomendacoesComponent },
+  { path: 'presentes', component: PresentesComponent },
+  { path: '**', component: NotFoundComponent } // Optional: Redirect unknown routes to home
+];
