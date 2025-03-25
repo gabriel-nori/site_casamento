5f931e84af045fa2bdebe4bdfd2ee5a35f6aac53
gabriel nori <gabrielnori@gabrielnori.tech>
2025-03-25 18:02:45 -0300

    Feat: Enable modal open from cart

--- gabe_e_giu/src/app/cart/cart.component.ts
+++ gabe_e_giu/src/app/cart/cart.component.ts
@@ -3,6 +3,8 @@ import { CartService } from '@services/cart.service'
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { product } from '@models/product.model';
+import { MatDialog } from '@angular/material/dialog';
+import { PresenteModalComponent } from '../presente-modal/presente-modal.component';
 
 @Component({
   selector: 'app-cart',
@@ -11,7 +13,10 @@ import { product } from '@models/product.model';
   styleUrl: './cart.component.css'
 })
 export class CartComponent implements OnInit{
-  constructor(private cart: CartService){
+  constructor(
+    private cart: CartService,
+    public dialog: MatDialog
+  ){
     this.cart_data = this.cart.get()
   }
 
@@ -22,5 +27,15 @@ export class CartComponent implements OnInit{
     for (const key in this.cart_data.items) {
       this.cart_items.push(this.cart_data.items[key])
     }
+  }
+
+  edit_item(item: CartProductInterface): void {
+    this.dialog.open(PresenteModalComponent, {
+      width: '90%',
+      height: '90%',
+      maxHeight: '750px',
+      maxWidth: '400',
+      data: item
+    });
   }
 }
--- gabe_e_giu/src/app/presente-modal/presente-modal.component.ts
+++ gabe_e_giu/src/app/presente-modal/presente-modal.component.ts
@@ -6,6 +6,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { product } from '@models/product.model';
 import { CartAddButtonComponent } from '../cart-add-button/cart-add-button.component';
+import { CartProductInterface } from '@models/cart.model';
 
 @Component({
   selector: 'app-presente-modal',
@@ -20,7 +21,7 @@ import { CartAddButtonComponent } from '../cart-add-button/cart-add-button.compo
 export class PresenteModalComponent {
   constructor(
     public dialogRef: MatDialogRef<PresenteModalComponent>,
-    @Inject(MAT_DIALOG_DATA) protected data: product
+    @Inject(MAT_DIALOG_DATA) protected data: product|CartProductInterface
   ) {}
 
   closeModal(): void {
