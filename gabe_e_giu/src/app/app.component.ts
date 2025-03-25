08b49b0440c611deda0f3881e5ecece7ba3c664c
gabriel.nori <gabrielnori@gabrielnori.tech>
2025-03-24 21:21:29 -0300

    Feat: Allow menu to open

--- gabe_e_giu/src/app/app.component.ts
+++ gabe_e_giu/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { MenuComponent } from './menu/menu.component'
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent],
+  imports: [CommonModule, RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'gabe_e_giu';
+  menu_open: boolean = false
+
+  toggle_menu() {
+    this.menu_open = !this.menu_open
+  }
 }
