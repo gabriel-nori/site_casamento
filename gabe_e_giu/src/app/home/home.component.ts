3627312dde4273c69dfe6abe00841664ebd37156
gabriel.nori <gabrielnori@gabrielnori.tech>
2025-03-27 20:00:02 -0300

    Feat: implement menu as component

--- gabe_e_giu/src/app/home/home.component.ts
+++ gabe_e_giu/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component'
+import { MenuComponent } from '../menu/menu.component';
 
 @Component({
   selector: 'app-home',
-  imports: [FooterComponent],
+  imports: [FooterComponent, MenuComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
--- gabe_e_giu/src/app/menu/menu.component.ts
+++ gabe_e_giu/src/app/menu/menu.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-menu',
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    CommonModule,
+    MatIconModule
+  ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
 export class MenuComponent {
+  menu_open: boolean = false
+
+  toggle_menu() {
+    this.menu_open = !this.menu_open
+  }
 
 }
