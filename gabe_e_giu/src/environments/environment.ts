ce7ee5736b6c154a625bb4c8298c5508c11959bc
gabriel.nori <gabrielnori@gabrielnori.tech>
2025-03-31 22:28:37 -0300

    Feat: create prod deploy settings

--- gabe_e_giu/src/environments/environment.ts
+++ gabe_e_giu/src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
-    production: false,
-    base_url: 'https://preview.gabeegiu.com.br/api/',
-    feature_flag: true,
-    default_cache_time_seconds: 15000, // use this for production: 15 * 60 * 1000
-    stripe_key: "pk_test_51R7RHtDHlDHjZElEsc0pI7bTfuwAXocUFDHN0wijQH0b5QFc19no05eCCGDresxKEtlvIMU8WeW8r7nYvjHsil2e00BDh1BzC4"
-  };
+  production: false,
+  base_url: 'https://preview.gabeegiu.com.br/api/',
+  feature_flag: true,
+  default_cache_time_seconds: 15000, // use this for production: 15 * 60 * 1000
+  stripe_key: "pk_test_51R7RHtDHlDHjZElEsc0pI7bTfuwAXocUFDHN0wijQH0b5QFc19no05eCCGDresxKEtlvIMU8WeW8r7nYvjHsil2e00BDh1BzC4"
+};
