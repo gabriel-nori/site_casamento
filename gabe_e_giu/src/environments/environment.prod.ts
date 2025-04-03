a2f6f2673bea5637569c7b0731934c78d80d7a68
gabriel.nori <gabrielnori@gabrielnori.tech>
2025-04-03 20:11:44 -0300

    Feat: Update cache time in production

--- gabe_e_giu/src/environments/environment.prod.ts
+++ gabe_e_giu/src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   base_url: 'https://gabeegiu.com.br/api/',
   feature_flag: true,
-  default_cache_time_seconds: 15 * 60 * 1000, // use this for production: 15 * 60 * 1000
+  default_cache_time_seconds: 2 * 60 * 1000, // use this for production: 15 * 60 * 1000
   stripe_key: "pk_live_51R7RHnDwbqcLMV0ggCJx5l74dz4Z5vuZULliG7vlUOjxT8m0hk37qixGrxT4I66bwNCSqU7dqHPJzL4ja207RXt000h28uMf3r"
 };
