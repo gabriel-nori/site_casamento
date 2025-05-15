8fe1d5d18126d5b9091d67e0472517e67fc949c5
gabriel.nori <gabrielnori@gabrielnori.tech>
2025-05-14 22:54:42 -0300

    Feat: Include a shitty layout just to show the info

--- gabe_e_giu/src/app/recomendacoes/recomendacoes.component.ts
+++ gabe_e_giu/src/app/recomendacoes/recomendacoes.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../menu/menu.component';
+import { MenuConfig } from '@models/menu.model';
+import { product } from '@models/product.model';
+import { CardPresenteComponent } from '../card-presente/card-presente.component';
 
 @Component({
   selector: 'app-recomendacoes',
-  imports: [MenuComponent],
+  imports: [
+    MenuComponent,
+    CardPresenteComponent
+  ],
   templateUrl: './recomendacoes.component.html',
   styleUrl: './recomendacoes.component.css'
 })
 export class RecomendacoesComponent {
-
+  logo_properties: MenuConfig = {
+    color: 'marsala',
+    background_color: "#f6b1ca",
+    hamburger_color: "#440f21"
+  }
+  product: product = {
+    category: "",
+    description: "Reserva de lugar na van para e ir voltar da festa",
+    id: 1234,
+    name: "Lugar na van para ir e voltar",
+    photo_path: "https://jvcvans.com.br/site/wp-content/uploads/2019/02/transporte-corporativo-sp.jpg",
+    price_cents: 4500,
+    type: ""
+  }
 }
